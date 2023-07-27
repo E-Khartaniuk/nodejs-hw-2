b6aa7be9523d570cdd0bf815bf7761c743088d53
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2023-07-27 12:07:12 +0300

    03 - mongo DB

---
+++ controllers/contactControllers.js
@@ -0,0 +1,110 @@
+const HttpError = require("../helpers");
+const isValidId = require("../middlewars/isValidId");
+const Contact = require("../models/contact");
+const {
+  contactUpdateFavoriteSchema,
+  contactsAddSchema,
+} = require("../schemas/contactsAddSchema");
+
+const getAll = async (req, res, next) => {
+  try {
+    // const results = await contactsService.listContacts();
+    const results = await Contact.find();
+    res.json(results);
+  } catch (error) {}
+};
+
+const getById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    isValidId;
+    const results = await Contact.findById(contactId);
+    if (!results) {
+      console.log(HttpError);
+      throw HttpError(404, `Not found`);
+    }
+    res.json(results);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const addContact = async (req, res, next) => {
+  try {
+    const { error } = contactsAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const results = await Contact.create(req.body);
+    res.status(201).json(results);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const results = await Contact.findByIdAndDelete(contactId);
+    if (!results) {
+      throw HttpError(404, `Contact with ID="${contactId} not found"`);
+    }
+    res.status(200).json({ message: "contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const changeById = async (req, res, next) => {
+  try {
+    const { error } = contactsAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { contactId } = req.params;
+    console.log(contactId);
+    const results = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
+    if (!results) {
+      console.log(HttpError);
+      throw HttpError(404, `Not found`);
+    }
+
+    res.json(results);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateFavorite = async (req, res, next) => {
+  try {
+    const { error } = contactUpdateFavoriteSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { contactId } = req.params;
+    // console.log(contactId);
+    const results = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
+    if (!results) {
+      console.log(HttpError);
+      throw HttpError(404, "missing field favorite");
+    }
+
+    res.json(results);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+  addContact,
+  deleteContact,
+  changeById,
+  updateFavorite,
+};
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,78 @@
-// const fs = require('fs/promises')
+// const fs = require("fs/promises");
+// const path = require("path");
+// const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+// const contactsPath = path.resolve("models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+// const listContacts = async () => {
+//   const data = await fs
+//     .readFile(contactsPath, "utf-8")
+//     .then((contacts) => contacts);
+//   const contactsArray = JSON.parse(data);
+//   return contactsArray;
+// };
 
-const removeContact = async (contactId) => {}
+// const getContactById = async (contactId) => {
+//   const data = await fs.readFile(contactsPath, "utf-8");
+//   const contactsArray = JSON.parse(data);
+//   const foundContact = contactsArray.find(
+//     (contact) => contact.id === contactId
+//   );
+//   if (typeof foundContact === "undefined") {
+//     console.log(null);
+//     return null;
+//   }
 
-const addContact = async (body) => {}
+//   console.log(foundContact);
+//   return foundContact;
+// };
 
-const updateContact = async (contactId, body) => {}
+// const removeContact = async (contactId) => {
+//   const contactById = await getContactById(contactId);
+//   if (!contactById) {
+//     return null;
+//   }
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+//   const contacts = await listContacts();
+//   const contactsWithoutDeleted = contacts.filter(
+//     (contact) => contact.id !== contactId
+//   );
+
+//   await fs.writeFile(
+//     contactsPath,
+//     JSON.stringify(contactsWithoutDeleted, null, 2)
+//   );
+
+//   return contactById;
+// };
+
+// const addContact = async (body) => {
+//   const allContacts = await listContacts();
+//   const newContact = {
+//     id: nanoid(),
+//     ...body,
+//   };
+//   allContacts.push(newContact);
+//   await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+
+//   return newContact;
+// };
+
+// const updateContact = async (id, { name, email, phone }) => {
+//   const contacts = await listContacts();
+//   const index = contacts.findIndex((item) => item.id === id);
+//   if (index === -1) {
+//     return null;
+//   }
+//   contacts[index] = { id, name, email, phone };
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//   return contacts[index];
+// };
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
